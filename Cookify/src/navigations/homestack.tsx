a41693a6c6630ae16d830afd95baf1e480470d34
YoungSudan <lochalamoicolin83@gmail.com>
2022-01-07 06:56:08 -0500

    Add react native navagationm, home scree stack

---
+++ Cookify/src/navigations/homestack.tsx
@@ -0,0 +1,12 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+const screens = {
+    Home: {
+        screen: 
+    }
+}
+
+const homeStack = createStackNavigator({
+
+})
